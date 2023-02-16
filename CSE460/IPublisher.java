bd9d13ffcffd0c511a69856c359ce3b21c57e0bd
Yazeed Alsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2023-02-16 19:44:37 +0300

    Delete CSE460 directory

--- CSE460/IPublisher.java
+++
@@ -1,7 +0,0 @@
-package ProducerBuyer;
-
-public interface IPublisher {
-
-	public abstract boolean publish(String prodCat, String modelType, String fuelType);
-
-}
--- CSE460/ISubscriber.java
+++
@@ -1,9 +0,0 @@
-package ProducerBuyer;
-
-public interface ISubscriber {
-
-	public abstract boolean subscribe(String prodCat);
-
-	public abstract boolean unsubscribe(String prodCat);
-
-}
